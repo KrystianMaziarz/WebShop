cefbc2dbe6da8e60d24937c0ea683d1dc8469d37
Jarosław Sobczak <jareksobczak9@gmail.com>
2019-12-15 12:41:00 +0100

    zmiana application.properties dla mysql

--- src/main/java/pl/com/store/webstore/controllers/CustomerController.java
+++ src/main/java/pl/com/store/webstore/controllers/CustomerController.java
@@ -22,26 +22,26 @@ public class CustomerController {
         return service.addCustomer(customerDto);
     }
 
-//    @GetMapping
-//    public ResponseEntity<List<UserDto>> findAll() {
-//        return ResponseEntity.ok(service.findAll().stream()
-//                .map(UserMapper::mapToDto)
-//                .collect(Collectors.toList()));
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<UserDto> findById(@PathVariable Long id) throws Exception {
-//        return ResponseEntity.ok(UserMapper.mapToDto(service.findById(id)));
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody UserDto userDto) throws Exception {
-//        return ResponseEntity.ok(UserMapper.mapToDto(service.updateUser(id, userDto)));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<String> deleteUser(@PathVariable Long id) throws Exception {
-//        service.deleteById(id);
-//        return ResponseEntity.ok("Deleted !");
-//    }
+    @GetMapping
+    public ResponseEntity<List<UserDto>> findAll() {
+        return ResponseEntity.ok(service.findAll().stream()
+                .map(UserMapper::mapToDto)
+                .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> findById(@PathVariable Long id) throws Exception {
+        return ResponseEntity.ok(UserMapper.mapToDto(service.findById(id)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody UserDto userDto) throws Exception {
+        return ResponseEntity.ok(UserMapper.mapToDto(service.updateUser(id, userDto)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) throws Exception {
+        service.deleteById(id);
+        return ResponseEntity.ok("Deleted !");
+    }
 }
